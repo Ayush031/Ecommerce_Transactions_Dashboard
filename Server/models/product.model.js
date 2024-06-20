d36ad5fecb2db0480e133c36ccb1a0f03f157069
Ayush Kansal <ayushkansal303@gmail.com>
2024-06-20 22:24:08 +0530

    Adding code

---
+++ Server/models/product.model.js
@@ -0,0 +1,16 @@
+import mongoose from "mongoose";
+
+const productSchema = new mongoose.Schema(
+    {
+        id: { type: String, required: true },
+        title: { type: String, required: true },
+        price: { type: Number, required: true },
+        description: { type: String, required: true },
+        category: { type: String, required: true },
+        image: { type: String, required: true },
+        sold: { type: Boolean, default: false },
+        dateOfSale: { type: Date }
+    }, { timestamps: true }
+);
+
+export const Product = mongoose.model('Product', productSchema);
