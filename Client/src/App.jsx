e0baacbc67bfd68d1f508f0c89118ca3ae5bc539
Ayush <ayushkansal303@gmail.com>
2024-07-24 19:51:58 +0530

    checking proxy server

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -60,7 +60,7 @@ function App() {
   function getCombineData(monthName, page, perPage) {
     axios
       .get(
-        `https://ecom-vercel-test-server.vercel.app/api/combined?monthName=${monthName}&page=${page}&perPage=${perPage}`
+        `/api/combined?monthName=${monthName}&page=${page}&perPage=${perPage}`
       )
       .then((response) => {
         if (!response) {
--- Client/vite.config.js
+++ Client/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {      
-      // '/api': 'https://ecom-vercel-test-server.vercel.app',
+      '/api': 'https://ecom-vercel-test-server.vercel.app',
     }
   },
   resolve: {
