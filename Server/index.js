1e195526f4084f6949b7e00098c6bc395c681a9d
Ayush <ayushkansal303@gmail.com>
2024-07-24 20:00:36 +0530

    fixed cors error

--- Server/index.js
+++ Server/index.js
@@ -11,10 +11,10 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
-// app.use(cors());
-app.use(cors({
-    origin: ['https://ecom-vercel-test-client.vercel.app'],
-}));
+app.use(cors());
+// app.use(cors({
+//     origin: ['https://ecom-vercel-test-client.vercel.app'],
+// }));
 app.use(express.urlencoded({ extended: true }));
 app.use('/api', router);
 
