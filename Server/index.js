8fba8013358e4fcc010ec725cd0ad48e63e464e9
Ayush <ayushkansal303@gmail.com>
2024-07-24 19:50:07 +0530

    checking cors

--- Server/index.js
+++ Server/index.js
@@ -11,15 +11,13 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
-app.use(cors());
-// app.use(cors({
-//     origin: ['https://ecom-vercel-test-client.vercel.app/api', 'http://localhost:3000'],
-// }));
+// app.use(cors());
+app.use(cors({
+    origin: ['https://ecom-vercel-test-client.vercel.app'],
+}));
 app.use(express.urlencoded({ extended: true }));
 app.use('/api', router);
 
 app.get('/', (req, res) => res.send('Server Started'));
 
-app.listen(PORT, () => {
-    console.log(`Server running on http://localhost:${PORT}`);
-});
+app.listen(PORT);
